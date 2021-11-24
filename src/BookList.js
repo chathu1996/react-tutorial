697c35e5803a3e5f764e4fddfa888c305727f352
chathu1996 <galahitiyawa@gmail.com>
2021-11-24 10:51:21 +0530

    iteration

--- src/BookList.js
+++ src/BookList.js
@@ -5,14 +5,22 @@ import "./BookList.css";
 
 const books = [
   {
+    id: 1,
     img: "https://m.media-amazon.com/images/I/91RVshgQn1S._AC_UL320_.jpg",
     title: "The Silent Patient",
     author: "Alex Michaelides, Jack Hawkins",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/91INn2ZEK+L._UX300__PJku-sticker-v7,TopRight,0,-50_AC_UL127_SR127,127_.jpg",
     title: "The Worst Best Friend: A Small",
     author: "Town Romance",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/31cPj2sYgIS._AC_SX184_.jpg",
+    title: "Dwyane",
+    author: "Dwyane Wade",
   },
 ];
 
@@ -20,8 +28,7 @@ function BookList() {
   return (
     <section className="bookList">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book book={book}></Book>;
+        return <Book key={book.id} book={book}></Book>;
       })}
     </section>
   );

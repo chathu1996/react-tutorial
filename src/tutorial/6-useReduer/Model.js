02f755ed773565786b1711344d924831e8751861
chathu1996 <galahitiyawa@gmail.com>
2021-12-04 18:28:17 +0530

    useReducer

--- src/tutorial/6-useReduer/Model.js
+++ src/tutorial/6-useReduer/Model.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Model = () => {
+const Model = ({ modalContent, closeModal }) => {
+    useEffect(()=>{
+        setTimeout(()=>{
+            closeModal()
+        }, 3000)
+    });
   return (
-    <div>
-      <h2>Model</h2>
+    <div className="modal">
+      <h2>{modalContent}</h2>
     </div>
   );
 };
--- src/tutorial/6-useReduer/index.js
+++ src/tutorial/6-useReduer/index.js
@@ -1,24 +1,34 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import Model from "./Model";
+import { reducer } from "./reducer";
 
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "Hello World",
+};
 const Index = () => {
   const [name, setName] = useState("");
-  const [people, setPeople] = useState();
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true);
-      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
       setName("");
     } else {
-      setShowModal(true);
+      dispatch({ type: "NO_VALUE" });
     }
+  };
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
   return (
     <div className="reducer-container">
-      {showModal && <Model />}
+      {state.isModalOpen && (
+        <Model closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -29,10 +39,17 @@ const Index = () => {
           <button type="submit">Add</button>
         </div>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
-          <div key="person.id">
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              Remove
+            </button>
           </div>
         );
       })}

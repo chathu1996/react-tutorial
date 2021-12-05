41cec1ad9608dd8fbb613cb2581968f585905c50
chathu1996 <galahitiyawa@gmail.com>
2021-12-05 22:02:27 +0530

    propDrilling basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "../src/tutorial/6-useReduer/UseReducer.css";
-import SetUp from "./tutorial/6-useReduer/index";
+import SetUp from "./tutorial/7-prop-drilling/1-prop-drilling";
 
 function App() {
   return (
